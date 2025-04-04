0ff3bf29d89523ebb515d654166619759945e3db
muwahhid <zelem139@gmail.com>
2025-04-04 15:00:50 +0300

    Настройка 100%

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import App from "./Components/App/App";
 
 const root = ReactDOM.createRoot(document.querySelectorAll(".root"));
 root.render(<App />);
